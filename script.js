4f3f9808c08d213d0eb9a53c03a9af55e581270b
estarossa <jestiwar.correa@gmail.com>
2021-07-20 20:15:27 -0500

    app staging

---
+++ script.js
@@ -0,0 +1,281 @@
+const API_key = "JeEMRksLDV54PQ3v7IELaJvwFiGLSKyudfabEYJz";
+
+let jsonRovers = {};
+
+let selectedRoverId = null;
+
+
+function getRoverFieldByKey(fieldKey, requiredField) {
+    const [key,value] = Object.entries(fieldKey)[0];
+
+    return jsonRovers.rovers.find( (rover) => rover[key] === value)[requiredField];
+
+    
+} 
+
+
+function clearSelect(className) {
+    var sel = document.querySelector(className);
+        for (i = sel.length - 1; i >= 0; i--) {
+	    sel.remove(i);
+    }
+}
+
+
+
+async function traerRovers(){
+
+    const apiRovers = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers?&api_key=${API_key}`);
+
+    jsonRovers = await apiRovers.json();
+
+   
+    jsonRovers.rovers.forEach(element => {
+
+        const $roverSelect = document.querySelector(".rover-select");
+
+        $option = document.createElement("option");
+        $option.setAttribute("value", element.name);
+        $option.textContent = element.name;
+
+        $roverSelect.appendChild($option)
+        
+    });
+
+    
+
+}
+
+
+function solNasa(){
+
+    
+    let $roverSelect = document.querySelector(".rover-select");
+
+    clearSelect(".select-sol");
+
+
+    if ($roverSelect.value !== "select") {
+
+        const $sol = document.querySelector(".sol");
+        $sol.style.display = "flex";
+
+
+        const maxSol = getRoverFieldByKey({name: $roverSelect.value}, "max_sol");
+
+
+        const $selectSol = document.querySelector(".select-sol");
+
+
+        for(i=0; i<=maxSol; i++) {
+            const option = document.createElement('option');
+            option.setAttribute('value', i);
+            option.innerHTML = i;
+            $selectSol.appendChild(option);
+        }
+
+
+        console.log($roverSelect.value,":" ,"Días máximos", maxSol)
+
+    } 
+      
+    let $solSelect = document.querySelector(".select-sol");
+
+    console.log("Sol seleccionado: ",$solSelect.value)
+     
+}
+
+
+function cameraNasa() {
+
+    const $cam = document.querySelector(".camera");
+    $cam.style.display = "flex";
+
+    let $roverSelect = document.querySelector(".rover-select");
+
+    clearSelect(".select-camera");
+
+
+    const $selectCamera = document.querySelector(".select-camera");
+
+    
+    
+    const cameras = getRoverFieldByKey({name:$roverSelect.value}, "cameras");
+
+    $option2 = document.createElement("option");
+    $option2.setAttribute("value", "cam");
+    $option2.textContent = "Select";
+    
+    $selectCamera.appendChild($option2)
+    
+
+    cameras.forEach(cam => {
+
+        $option = document.createElement("option");
+        $option.setAttribute("value", cam.name);
+        $option.textContent = cam.full_name;
+        
+        $selectCamera.appendChild($option)
+  
+    });
+
+}
+
+const $modal = document.querySelector(".modal");
+
+const $closePhotos = document.querySelector(".modal > button");
+
+
+
+
+async function getPhotos() {
+   try {
+
+       let $roverSelect = document.querySelector(".rover-select").value;
+    
+       const $selectSol = document.querySelector(".select-sol").value;
+    
+       const $selectCamera = document.querySelector(".select-camera").value;
+
+       
+    
+       const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${$roverSelect}/photos?sol=${$selectSol}&camera=${$selectCamera}&api_key=${API_key}`;
+    
+       console.log(url);
+
+       const response = await fetch(url);
+
+       const {photos} = await response.json();
+
+       const $contNoPhotos = document.querySelector(".container-no-photos");
+
+       const $contDown = document.querySelector(".container-down");
+
+       console.log("fotos: ",photos, photos.length);
+
+
+       //---------------------------------------------------------------
+       function carrousel($contDown){
+        $contDown.addEventListener("click", el => {
+               let atras = $contDown.querySelector(".atras");
+               let adelante = $contDown.querySelector(".adelante");
+               let img = $contDown.querySelector("img");
+               let tgt = el.target;
+
+               if (tgt === atras){
+
+                   if (cont > 0){
+                       img.src = myArray[cont - 1];
+                       cont--;
+                   }else {
+                       img.src = myArray[myArray.length - 1];
+                       cont = myArray.length - 1;
+                   }
+
+               }else if (tgt === adelante) {
+                if (cont < myArray.length - 1){
+                    img.src = myArray[cont + 1];
+                    cont++;
+                }else {
+                    img.src = myArray[0];
+                    cont = 0;
+                }
+               }
+           })
+       }
+       //-------------------------------------------------------------
+
+       $contDown.innerHTML = "";
+       $contNoPhotos.style.display = "none";
+       
+       if (photos.length > 0) {
+
+
+           
+           
+           photos.forEach(({img_src}, index) => {
+              $img = document.createElement("img");
+              $img.setAttribute("src", img_src);
+              $img.setAttribute("alt", "camera image");
+              $img.className = "image-rover";
+              $img.setAttribute("id", index);
+
+                           
+              $contDown.appendChild($img);
+           })
+
+
+           $nodeList = document.querySelectorAll(".container-down img");
+
+           let myArray = Array.from($nodeList);
+
+           //let cont = 0;
+
+           console.log("Lista", myArray);
+
+           
+
+
+          
+           $contDown.addEventListener("click", (event) => {
+               if (event.target.tagName.toLowerCase() === "img") {
+
+                   
+                   $contenedor = document.querySelector(".contenedor");
+                   $modal.style.display = "flex";
+                   $contenedor.style.display = "flex";
+
+                   
+
+                   $contenedor.appendChild(event.target);
+
+                   $modal.appendChild($contenedor);
+
+                   $closePhotos.addEventListener("click", () => {
+                    $modal.style.display = "none";
+                    $contDown.appendChild(event.target);
+
+                })
+                   
+               }
+            
+           
+          })
+
+       }else {
+        $contNoPhotos.style.display = "flex";   
+
+
+        $noPhotos = document.querySelector(".no-photos");
+        $noPhotos.textContent = "There are not photos in this camera";
+
+
+        $imgNoPhotos = document.querySelector(".img-no-photos");
+        $imgNoPhotos.style.display = "flex";
+
+       }
+
+
+  
+
+   }catch(error){
+       console.log(error)
+   }
+}
+
+window.onscroll = function() {
+    if (window.scrollY > 600) {
+        const $up = document.querySelector(".up");
+        $up.style.visibility = "visible";
+    
+    } else if (window.scrollY < 600) {
+        const $up = document.querySelector(".up");
+        $up.style.visibility = "hidden";
+    }
+};
+
+
+
+traerRovers();
+
+
